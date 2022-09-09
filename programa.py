23ea1abfb678794d9b8d829abcc11a2c3abc884a
h-r0rsch4ch <70857940+h-r0rsch4ch@users.noreply.github.com>
2022-09-09 01:45:17 -0500

    Update programa.py

--- programa.py
+++ programa.py
@@ -5,16 +5,17 @@ import re
 
 # Boton Borrar
 def delete():
-    inputCadena.delete(0,'end')
+    inputCadena.delete(0, 'end')
+
 
 # Validacion de la cadena
 def validacionCadena():
     regex = re.compile(r"([0-4]+)(\.)?((a|c|d|e|h|i|m|n|o|q|r|u|[0-4]+)+(\.)?)*(erm)+(\.)?((a|c|d|e|h|i|m|n|o|q|r|u|[0-4]+)+(\.))*2034212")
     cadena = str(inputCadena.get())
     if re.fullmatch(regex, cadena):
-        etiquetaResultado.config(text=f"La cadena {cadena} es valida.\n\n Presione borra para una nueva cadena")
+        etiquetaResultado.config(text=f"La cadena {cadena} es valida.\n\nPresione el boton Borrar e ingrese una nueva cadena.")
     else:
-        etiquetaResultado.config(text=f"La cadena {cadena} no es valida.\n\n Presione borra para una nueva cadena")
+        etiquetaResultado.config(text=f"La cadena {cadena} no es valida.\n\nPresione el boton Borrar e ingrese una nueva cadena.")
 
 # Configuracion de la ventana
 ventana = tk.Tk()
@@ -29,16 +30,16 @@ etiquetaCadena.place(x=20, y=20)
 inputCadena = ttk.Entry()
 inputCadena.place(x=22, y=40, width=360)
 
-# Boton
+# Boton Comprobar
 botonCadena = ttk.Button(text="Comprobar", command=validacionCadena)
 botonCadena.place(x=22, y=65)
 
 # Boton Borrar
 botonBorrar = ttk.Button(text="Borrar", command=delete)
-botonBorrar.place(x=22, y=80)
+botonBorrar.place(x=110, y=65)
 
 # Resultado Validacion
 etiquetaResultado = ttk.Label(text="")
-etiquetaResultado.place(x=22, y=130)
+etiquetaResultado.place(x=22, y=110)
 
 ventana.mainloop()
